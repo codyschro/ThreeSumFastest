0feec14328340e290c4b480cac872c713d010d23
codyschroeder@hotmail.com <H6u0r5on>
2019-09-27 21:25:49 -0500

    Initial commit

---
+++ src/ThreeSumFastest.java
@@ -0,0 +1,130 @@
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.io.*;
+import java.util.*;
+
+public class ThreeSumFastest {
+    static ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+
+    /* define constants */
+    static long MAXVALUE =  2000000000;
+    static long MINVALUE = -2000000000;
+    static int numberOfTrials = 10;
+    static int MAXINPUTSIZE  = (int) Math.pow(2,17);
+    static int MININPUTSIZE  =  1;
+    // static int SIZEINCREMENT =  10000000; // not using this since we are doubling the size each time
+    static String ResultsFolderPath = "/home/codyschroeder/Results/"; // pathname to results folder
+    static FileWriter resultsFile;
+    static PrintWriter resultsWriter;
+
+    public static void main(String[] args) {
+        // run the whole experiment at least twice, and expect to throw away the data from the earlier runs, before java has fully optimized
+        System.out.println("Running first full experiment...");
+        runFullExperiment("ThreeSumFastest-Exp1-ThrowAway.txt");
+        System.out.println("Running second full experiment...");
+        runFullExperiment("ThreeSumFastest-Exp2.txt");
+        System.out.println("Running third full experiment...");
+        runFullExperiment("ThreeSumFastest-Exp3.txt");
+    }
+
+    static void runFullExperiment(String resultsFileName){
+
+        try {
+            resultsFile = new FileWriter(ResultsFolderPath + resultsFileName);
+            resultsWriter = new PrintWriter(resultsFile);
+
+        } catch(Exception e) {
+            System.out.println("*****!!!!!  Had a problem opening the results file "+ResultsFolderPath+resultsFileName);
+            return; // not very foolproof... but we do expect to be able to create/open the file...
+        }
+
+        ThreadCpuStopWatch BatchStopwatch = new ThreadCpuStopWatch(); // for timing an entire set of trials
+        ThreadCpuStopWatch TrialStopwatch = new ThreadCpuStopWatch(); // for timing an individual trial
+
+        resultsWriter.println("#InputSize    AverageTime"); // # marks a comment in gnuplot data
+        resultsWriter.flush();
+        /* for each size of input we want to test: in this case starting small and doubling the size each time */
+        for(int inputSize=MININPUTSIZE;inputSize<=MAXINPUTSIZE; inputSize*=2) {
+            // progress message...
+            System.out.println("Running test for input size "+inputSize+" ... ");
+            /* repeat for desired number of trials (for a specific size of input)... */
+            long batchElapsedTime = 0;
+            /* force garbage collection before each batch of trials run so it is not included in the time */
+            System.gc();
+            // instead of timing each individual trial, we will time the entire set of trials (for a given input size)
+            // and divide by the number of trials -- this reduces the impact of the amount of time it takes to call the
+            // stopwatch methods themselves
+            //BatchStopwatch.start(); // comment this line if timing trials individually
+
+            // run the trials
+            for (long trial = 0; trial < numberOfTrials; trial++) {
+                // generate a random list of integers for each trial
+                System.out.print("    Generating test data...");
+                long[] newList = createRandomListOfIntegers(inputSize);
+                System.out.println("...done.");
+                System.out.print("    Running trial batch...");
+                /* force garbage collection before each trial run so it is not included in the time */
+                System.gc();
+
+                TrialStopwatch.start(); // *** uncomment this line if timing trials individually
+                /* run the function we're testing on the trial input */
+                int count = threeSumFastest(newList);
+                batchElapsedTime = batchElapsedTime + TrialStopwatch.elapsedTime(); // *** uncomment this line if timing trials individually
+            }
+
+            //batchElapsedTime = BatchStopwatch.elapsedTime(); // *** comment this line if timing trials individually
+            double averageTimePerTrialInBatch = (double) batchElapsedTime / (double)numberOfTrials; // calculate the average time per trial in this batch
+
+            /* print data for this size of input */
+            resultsWriter.printf("%12d  %15.2f \n",inputSize, averageTimePerTrialInBatch); // might as well make the columns look nice
+            resultsWriter.flush();
+            System.out.println(" ....done.");
+        }
+    }
+
+    public static int threeSumFastest(long[] list){
+
+        //set variables
+        int N = list.length;
+        int count = 0;
+
+        //sort the list
+        Arrays.sort(list);
+
+        //use single loop to cycle thru elements
+        for(int i = 0; i < N-1; i++){
+            //set left and right variables at each end of the list, work in from there, set x to list[i]
+            int left = i + 1;
+            int right = N - 1;
+            long x = list[i];
+            //while left is to left of right
+            while(left < right){
+                //if the triplet sums to zero, increase count
+                if(x + list[left] + list[right] == 0){
+                    count++;
+                    //increase left index and decrease right index
+                    left++;
+                    right--;
+                }
+                //if sum is less than zero, increase left
+                else if(x + list[left] + list[right] < 0){
+                    left++;
+                }
+                //else decrease right index
+                else{
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+
+    //create random integer list containing negative and positive numbers
+    public static long[] createRandomListOfIntegers(int size){
+        long[] newList = new long[size];
+        for(int j = 0; j < size; j++) {
+            newList[j] = (long) (MINVALUE + Math.random() * (MAXVALUE - MINVALUE));
+        }
+        return newList;
+    }
+}
